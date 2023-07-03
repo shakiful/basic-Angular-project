7fa2db2ffbab8d65e09163f62666cb20d3463fe4
Shakiful <shakifulkhan@gmail.com>
2023-07-03 12:41:21 +0600

    recipe & shopping module separation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,56 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeComponent } from './recipe/recipe.component';
-import { RecipeStartComponent } from './recipe/recipe-start/recipe-start.component';
-import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
-import { MemberAuthGuard } from './isAdmin.guard';
-import { PlatinumMemberAuthGuard } from './isPlatinumMember.guard';
-import { SuperAdminGuard } from './isSuper.guard';
-import { RecipesResolverService } from './recipe/recipe-resolver.service';
 import { AuthComponent } from './auth/auth.component';
-import { authGuardFn } from './auth/auth-guard';
 
-const routes: Routes = [
+const appRoutes: Routes = [
   { path: '', redirectTo: '/recipe', pathMatch: 'full' },
-  {
-    path: 'recipe',
-    component: RecipeComponent,
-    canActivate: [authGuardFn],
-    // canActivate: [MemberAuthGuard, PlatinumMemberAuthGuard],
-    // canActivateChild: [SuperAdminGuard],
-    // canDeactivate: [() => true],
-    children: [
-      {
-        path: '',
-        component: RecipeStartComponent,
-      },
-      {
-        path: 'new',
-        component: RecipeEditComponent,
-      },
-      {
-        path: ':id',
-        component: RecipeDetailComponent,
-        resolve: [RecipesResolverService],
-      },
 
-      {
-        path: ':id/edit',
-        component: RecipeEditComponent,
-        resolve: [RecipesResolverService],
-      },
-    ],
-  },
-  { path: 'shopping-list', component: ShoppingListComponent },
   {path: 'auth', component: AuthComponent},
-  { path: 'not-found', component: ShoppingListComponent },
-  { path: '**', redirectTo: 'not-found' },
+  // { path: 'not-found', component: ShoppingListComponent },
+  // { path: '**', redirectTo: 'not-found' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  HttpClient,
+  HttpClientModule,
+} from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirectiveTsDirective } from './shared/dropdown.directive.ts.directive';
 import { RecipeService } from './recipe/recipe.service';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
@@ -17,13 +19,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor';
+import { RecipeModule } from './recipe/recipe.module';
+import { ShoppingModule } from './shopping-list/shopping.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     DropdownDirectiveTsDirective,
     AuthComponent,
     LoadingSpinnerComponent,
@@ -33,9 +35,22 @@ import { AuthInterceptorService } from './auth/auth-interceptor';
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RecipeModule,
+    ShoppingModule,
   ],
-  providers: [RecipeService,ShoppingListService, MemberAuthGuard, PlatinumMemberAuthGuard, SuperAdminGuard, {provide: HTTP_INTERCEPTORS, useClass:AuthInterceptorService, multi:true}],
-  bootstrap: [AppComponent]
+  providers: [
+    RecipeService,
+    ShoppingListService,
+    MemberAuthGuard,
+    PlatinumMemberAuthGuard,
+    SuperAdminGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
