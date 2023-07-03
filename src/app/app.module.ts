3f886acb1f1edcbc6470afbe9b70028ae58921b4
Shakiful <shakifulkhan@gmail.com>
2023-07-03 11:34:53 +0600

    recipe-module creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { RecipeComponent } from './recipe/recipe.component';
-import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
-import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
 import { DropdownDirectiveTsDirective } from './shared/dropdown.directive.ts.directive';
 import { RecipeService } from './recipe/recipe.service';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RecipeStartComponent } from './recipe/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
 import { MemberAuthGuard } from './isAdmin.guard';
 import { PlatinumMemberAuthGuard } from './isPlatinumMember.guard';
 import { SuperAdminGuard } from './isSuper.guard';
@@ -30,13 +24,7 @@ import { AuthInterceptorService } from './auth/auth-interceptor';
     HeaderComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    RecipeComponent,
-    RecipeDetailComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
     DropdownDirectiveTsDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
   ],
